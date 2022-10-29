0a842650a464c92ec30d161b1487ca4b98cf5885
Matthew Campbell <matt@123elm.com>
2022-10-29 09:06:51 -0400

    Update energy provider with CPU usage (#25)
    * rename heuristic energy provider to match industry language 'proxy'
    
    * Updated resource usage energy provider with CPU metrics

--- carbon-aware-starter/src/main/java/com/carbonaware/config/CarbonAwareAutoConfiguration.java
+++ carbon-aware-starter/src/main/java/com/carbonaware/config/CarbonAwareAutoConfiguration.java
@@ -5,7 +5,7 @@ import com.carbonaware.apis.CarbonEmissionsParams;
 import com.carbonaware.apis.DefaultCarbonEmissionsParams;
 import com.carbonaware.endpoint.CarbonAwareActuatorEndpoint;
 import com.carbonaware.sci.*;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -45,8 +45,8 @@ public class CarbonAwareAutoConfiguration {
     }
 
     @Bean
-    public EnergyConsumptionProvider energyConsumptionProvider() {
-        return new HeuristicEnergyConsumptionProvider();
+    public EnergyConsumptionProvider energyConsumptionProvider(MeterRegistry metricsRegistry) {
+        return new ResourceUtilizationEnergyConsumptionProvider(metricsRegistry);
     }
 
     @Bean
